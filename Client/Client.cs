b637b25ea80f30cc8d18deebd0caa5dc2f0524ff
OCY <ok0523ok@gmail.com>
2024-06-19 01:28:42 +0900

    공부

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,71 +1,78 @@
-﻿using System.Net.Sockets;
-using System.Net;
+﻿using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading;
 
-namespace udp_03
+namespace console_tcpClient_variableType2
 {
-    internal class Program
+    internal class Client_variableType2
     {
-        public static Socket sock;
-
         static void Main(string[] args)
         {
-            var remoteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 25000);
-            ConnectedUDP(remoteEP);
+            Console.WriteLine("[CLIENT] Send Message VariableType 2 With SizeData");
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Loopback, 25000);
+            SendMessageVariableTypeWithSizeData(remoteEP);
         }
 
-        private static void ConnectedUDP(IPEndPoint remoteEP)
+        private static void SendMessageVariableTypeWithSizeData(IPEndPoint remoteEP)
         {
-            // 소켓 생성
-            sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.SendBuffer, 5);
 
-            // 바인드 (선택이긴 함)
-            sock.Bind(new IPEndPoint(IPAddress.Loopback, 25001));
+            sock.Connect(remoteEP);
 
-            // 스레드 시작: 데이터를 지속적으로 수신하는 부분을 별도의 스레드에서 실행
-            Thread receiverThread = new Thread(ReceiveData);
-            receiverThread.Start();
+            // 사이즈+ 데이터 분리 전송용
+            String sendCommand = "GET Weather/NewTemp";
+            byte[] DataBuf = Encoding.UTF8.GetBytes(sendCommand);
+            byte[] sizeBuf = new byte[sizeof(int)];
+            sizeBuf = BitConverter.GetBytes(DataBuf.Length);
 
-            // 사용자 입력을 지속적으로 송신
-            while (true)
-            {
-                string userInput = Console.ReadLine();
-                if (string.IsNullOrEmpty(userInput))
-                {
-                    break;
-                }
-                byte[] buffer = Encoding.UTF8.GetBytes(userInput);
-                int retval = sock.SendTo(buffer, buffer.Length, SocketFlags.None, remoteEP);
-            }
+            // 사이즈+데이터 통합 전송용
+            String sendCommand2 = "POST seoul/Wind";
+            byte[] DataBuf2 = Encoding.UTF8.GetBytes(sendCommand2);
+            byte[] sizeBuf2 = new byte[sizeof(int)];
+            sizeBuf2 = BitConverter.GetBytes(DataBuf2.Length);
+            // 호스트 2 네트워크 바이트 오더 바이트 순서변경
+            // 수업중에 필요 없고 모든 컴퓨터가 같은 intel 칩이여서 넘어감.
+            // 하지만 꼭 함수  확인할것
 
-            // 프로그램이 종료되면 소켓 닫기
-            sock.Close();
-        }
+            // 이버전은 일반  소켓 테스트 툴로는 확인 불가능
+            // 일반 소켓 테스터들은 사이즈 확인 처리부분이 없음.
 
-        private static void ReceiveData()
-        {
-            // 수신 준비
-            var receiver = new IPEndPoint(IPAddress.Any, 0);
-            EndPoint recRef = (EndPoint)receiver;
+            // 1.분리전송
+            // SizeBuf 전송
+            sock.Send(sizeBuf, 0, sizeBuf.Length, SocketFlags.None);
+            // DataBuf 전송
+            sock.Send(DataBuf, 0, DataBuf.Length, SocketFlags.None);
 
-            // 지속적으로 데이터를 받기 위한 루프
-            while (true)
-            {
-                byte[] buffer = new byte[1024];
-                Console.WriteLine("Before receiveFrom ~~");
-                // 동기식 함수로 데이터 수신이 없으면 Blocking 상태를 유지하고 더이상 코드가 진행되지 않는다
-                int recvbyte = sock.ReceiveFrom(buffer, ref recRef);
-                Console.WriteLine($"After receiveFrom [{recvbyte}] bytes");
-                Console.WriteLine($"[Server] {Encoding.UTF8.GetString(buffer, 0, recvbyte)}");
-                // UDP는 연결이 끝났는지 모른다.
-                // 데이터가 없는 패킷을 보냈을때만 반응
-                if (recvbyte == 0)
-                {
-                    Console.WriteLine("Closed from server~!");
-                    return;
-                }
-            }
+            // 2.통합전송
+            //byte[] fullBytes = new byte[DataBuf2.Length + sizeBuf2.Length];
+            //Array.Copy(sizeBuf2, fullBytes, sizeBuf2.Length);
+            //Array.Copy(DataBuf2, 0, fullBytes, sizeBuf2.Length, DataBuf2.Length);
+            //sock.Send(fullBytes);
+
+            // 수업중 통합 전송 지연 시뮬레이션은
+            // 수신측에서 데이터 재결합을 시연하기 위해서 작성한 것으로 
+            // 이 코드에서 제외하였음.
+            // 2-1. 통합 전송 지연 시뮬레이션
+            // 2. 통합 전송부분 주석처리하여 사용바람.
+            // 합배송 지연 시뮬레이션 
+            byte[] spliteBuf1 = new byte[5];
+            byte[] spliteBuf2 = new byte[5];
+            byte[] spliteBuf3 = new byte[5];
+            byte[] spliteBuf4 = new byte[5];
+            Array.Copy(DataBuf2, 0, spliteBuf1, 0, spliteBuf1.Length);
+            Array.Copy(DataBuf2, spliteBuf1.Length, spliteBuf2, 0, spliteBuf2.Length);
+            Array.Copy(DataBuf2, spliteBuf1.Length * 2, spliteBuf3, 0, spliteBuf3.Length);
+            //Array.Copy(DataBuf2, spliteBuf1.Length*3, spliteBuf4, 0, spliteBuf4.Length);
+
+            sock.Send(sizeBuf2, 0, sizeBuf.Length, SocketFlags.None);
+            Thread.Sleep(1000); // 데이터 지연 효과
+            sock.Send(spliteBuf1);
+            Thread.Sleep(1000); // 데이터 지연 효과
+            sock.Send(spliteBuf2);
+            Thread.Sleep(1000); // 데이터 지연 효과
+            sock.Send(spliteBuf3);
+            Thread.Sleep(1000);
         }
     }
 }
--- ConsoleApp1/Server.cs
+++ ConsoleApp1/Server.cs
@@ -8,94 +8,48 @@ namespace Lean
     internal class Server
     {
         private static Socket sock;
+        private static IPEndPoint serverEP;
 
         static void Main(string[] args)
         {
-            // 로컬 네트워크 테스트용 EndPoint 설정
-            var remoteEP = new IPEndPoint(IPAddress.Loopback, 25000);
-            var remoteEP2 = new IPEndPoint(IPAddress.Loopback, 25000);
-            // TCP 클라이언트로 메세지 전송
-            SendTcpMsg(remoteEP, "Hi\n");
-            SendTcpMdg2(remoteEP2, "Hi\n");
-
-            //// DNS 를 이용한 네트워크 전송 설정
-            // var hostEntry = Dns. GetHostEntry("www.google.com");
-            // hostEntry.AddressList 에서 확인된 IP 정보로 EndPoint 설정
-            // var remoteEp = new IPEndPoint(hostEntry.AddressList[0], 80);
-            //// TCP 클라이언트로 메세지 전송
-            //// 차이점 웹서버가 인식가능한 문장으로 전송
-            //SendTcpMsg(remoteEP, "GET / HTTP/1.1\r\n\r\n");
+            serverEP = new IPEndPoint(IPAddress.Any, 25000);
+            ServerStart(serverEP);
         }
 
-        private static void SendTcpMdg2(IPEndPoint remoteEP2, string v)
+        private static void ServerStart(IPEndPoint serverEP)
         {
-            try
-            {
-                //Socket 생성
-                Console.WriteLine("[info] -- Create Skcket");
-                sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                //접속 시도
-
-            }
-            catch 
-            {
-                
-            }
+            // 생성
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            // 바인드
+            sock.Bind(serverEP);
+            // 스레드로 클라이언트 받기
+            Thread recThread = new Thread(RecStart);
+            recThread.Start();
         }
 
-        private static void SendTcpMsg(IPEndPoint remoteEP, string v)
+        private static void RecStart()
         {
-            try 
+            // 수신 준비 클라이언트 EP
+            var recipEP = new IPEndPoint(IPAddress.Any, 0);
+
+            // 수신 준비 버퍼
+            byte[] buffer = new byte[1024];
+
+            while (true)
             {
-                // Socket 생성
-                Console.WriteLine("[info] -- Create Socket");
-                sock = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
-                // 접속 시도
-                Console.WriteLine($"[info] -- Connect to [{remoteEP.Address}] : [{remoteEP.Port}]");
-                sock.Connect(remoteEP); // 머하는넘이지
-                Console.WriteLine("[info] -- Connected !!");
+                // 수신 준비 리시브프롬
+                EndPoint clientEP = (EndPoint)recipEP;
 
-                // 데이터 준비, 변경 string to Byte[]
-                byte[] buffer = Encoding.UTF8.GetBytes(v);
-                int size = buffer.Length;
+                // 수신
+                int retval = sock.ReceiveFrom(buffer, ref clientEP);
+                string retstring = Encoding.UTF8.GetString(buffer,0,retval);
 
-                // 버퍼 내용 확인
-                // String 클래스와 다르게 바이트배열은 클래스가 가지고 있는 기본 정보는 타입 정보이며
-                // 인코딩을 통해서 내용을 확인 가능. 그니까 그냥 찍으면 타입 정보 알려줌
-                Console.WriteLine($"[data] ==> [{Encoding.UTF8.GetString(buffer)}]");
+                // 수신 확인
+                Console.WriteLine($"[recdata] retval: {retval} / retstring: {retstring}");
 
-                // 데이터 송신
-                int retval = sock.Send(buffer,0,size, SocketFlags.None); // 소켓플래그 머하는넘이지
-
-                // 데이터 수신
-                // 이더넷 최대 데이터 사이즈 MTU == 1500Bytes
-                // TCP에서는 작아도 문제는 없음.
-                // UDP에서는 수신 버퍼 사이즈가 작으면 예외 발생
-                buffer = new byte[1500];
-                retval = sock.Receive(buffer, 0, buffer.Length, SocketFlags.None);
-                Console.WriteLine($"[info] -- recvButes[{retval}]");
-
-                // 데이터 처리, 바이트 확인, 수정, 삭제 등등
-                for(int i =0; i< retval; i++)
-                {
-                    if (buffer[i] == 0x0d || buffer[i]== 0x0a)
-                    {
-                        buffer[i] = 0x21;
-                    }
-                }
-
-                // 수신 데이터 최종 확인
-                Console.WriteLine($"[data] ==> [{Encoding.UTF8.GetString(buffer)}]");
-                Console.WriteLine("[info] ==> [Finish Recv]");
-
+                // 에코
+                sock.SendTo(buffer, buffer.Length, SocketFlags.None, clientEP);
             }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            } 
-            Console.WriteLine("[info] -- socket closed");
-            sock.Close();
-
         }
     }
 }
